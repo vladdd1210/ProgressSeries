f9e02b1ed61d0b2d11baeac80235fa84642b23cd
vladdd1210 <vladvovchenko2000@gmail.com>
2020-05-20 18:43:19 +0300

    RAW FILES

--- JS/changetype.js
+++
@@ -1,3 +0,0 @@
-function redirect(epValue){
-    document.getElementById("changeEpisode").submit();
-}
--- RAW FILES/WebContent/WEB-INF/Classes/Compile.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-javac -classpath "C:\Apache\lib\servlet-api.jar" -d "C:\Apache\webapps\ROOT\WEB-INF\classes" -sourcepath ..\..\..\src *.java
-pause
